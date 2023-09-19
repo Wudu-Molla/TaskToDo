92fb6654d23b4f2c41a774b2fe31433eb4f03895
Wudu-Molla <wudu.molla11@gmail.com>
2023-09-19 11:30:29 +0300

    designed the transaction of the fragments, chnged the color of the home_selected icon

--- app/src/main/java/com/example/taskstodo/Home.java
+++ app/src/main/java/com/example/taskstodo/Home.java
@@ -1,14 +1,12 @@
 package com.example.taskstodo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-
-import android.app.Notification;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class Home extends AppCompatActivity implements View.OnClickListener {
@@ -31,11 +29,60 @@ public class Home extends AppCompatActivity implements View.OnClickListener {
         tasksText = findViewById(R.id.tasksText);
         homeText = findViewById(R.id.homeText);
         notificationText = findViewById(R.id.notificationText);
+        home.setOnClickListener(this);
+        notification.setOnClickListener(this);
+        tasks.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.tasks:
+                tasks.setBackground(getDrawable(R.drawable.rounded));
+                tasksText.setVisibility(View.VISIBLE);
 
+                home.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+                notification.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+
+                homeText.setVisibility(View.GONE);
+                notificationText.setVisibility(View.GONE);
+
+                homeIcon.setImageResource(R.drawable.home);
+                notificationIcon.setImageResource(R.drawable.notification);
+                manager.beginTransaction().setReorderingAllowed(true)
+                        .add(R.id.fragment_container, TasksFragment.class, null).commit();
+                break;
+            case R.id.notification:
+                tasks.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+                tasksText.setVisibility(View.GONE);
+
+                notification.setBackground(getDrawable(R.drawable.rounded));
+                home.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+
+                homeText.setVisibility(View.GONE);
+                notificationText.setVisibility(View.VISIBLE);
+
+                homeIcon.setImageResource(R.drawable.home);
+                notificationIcon.setImageResource(R.drawable.notification_selected);
+                manager.beginTransaction().setReorderingAllowed(true)
+                        .add(R.id.fragment_container, NotificationFragment.class, null).commit();
+            break;
+            default:
+                manager.beginTransaction().setReorderingAllowed(true)
+                        .add(R.id.fragment_container, HomeFragment.class, null).commit();
+                tasks.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+                tasksText.setVisibility(View.GONE);
+
+                home.setBackground(getDrawable(R.drawable.rounded));
+                notification.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+
+                homeText.setVisibility(View.VISIBLE);
+                notificationText.setVisibility(View.GONE);
+
+                homeIcon.setImageResource(R.drawable.home_selected);
+                notificationIcon.setImageResource(R.drawable.notification);
+            break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/taskstodo/TasksFragment.java
@@ -0,0 +1,25 @@
+package com.example.taskstodo;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TasksFragment extends Fragment {
+    private View view;
+
+    public TasksFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_tasks, container, false);
+        return view;
+    }
+}
