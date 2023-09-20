bb4adefe95ac465bafebf5033b8c19226c87aae1
Wudu-Molla <lawudula2@gmail.com>
2023-09-20 21:03:06 +0300

    loaded the resource

--- app/src/main/java/com/example/taskstodo/TaskViewHolder.java
+++ app/src/main/java/com/example/taskstodo/TaskViewHolder.java
@@ -1,13 +1,23 @@
 package com.example.taskstodo;
 
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class TaskViewHolder extends RecyclerView.ViewHolder {
 
+
+    CheckBox isDone;
+    TextView task, end_date, remaining_time;
+
     public TaskViewHolder(@NonNull View itemView) {
         super(itemView);
+        isDone = itemView.findViewById(R.id.is_done);
+        task = itemView.findViewById(R.id.task);
+        end_date = itemView.findViewById(R.id.end_date);
+        remaining_time = itemView.findViewById(R.id.remaining_time);
     }
 }
