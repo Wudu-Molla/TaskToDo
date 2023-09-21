f1d4b1cf2a6b96a807b7016a2de05ee0e17de468
Wudu-Molla <lawudula2@gmail.com>
2023-09-21 10:23:10 +0300

    handled the data retrived from the database and presented on the recycleview on the home fragment

--- app/src/main/java/com/example/taskstodo/HomeFragment.java
+++ app/src/main/java/com/example/taskstodo/HomeFragment.java
@@ -1,15 +1,23 @@
 package com.example.taskstodo;
 
+import android.database.Cursor;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.taskstodo.db.DBManager;
+
 public class HomeFragment extends Fragment {
     View view;
+    RecyclerView recyclerView;
+    Cursor cursor;
+    DBManager dbManager;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -21,6 +29,14 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_home, container, false);
+        recyclerView = view.findViewById(R.id.task_collection);
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireActivity()));
+        dbManager = new DBManager(requireActivity());
+        dbManager.openDB();
+        dbManager.insert("Buy milk", "21/9/2023", "true");
+        cursor = dbManager.getData();
+        recyclerView.setAdapter(new TaskAdapter(requireActivity(), cursor));
+
         return view;
     }
 }
--- app/src/main/java/com/example/taskstodo/TaskAdapter.java
+++ app/src/main/java/com/example/taskstodo/TaskAdapter.java
@@ -1,18 +1,26 @@
 package com.example.taskstodo;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.taskstodo.db.DBManager;
+
 public class TaskAdapter extends RecyclerView.Adapter<TaskViewHolder> {
 
     private Context context;
+    Cursor cursor;
 
-    public TaskAdapter(Context context) {
+
+    public TaskAdapter(Context context, Cursor cursor) {
         this.context = context;
+        this.cursor = cursor;
+
+
     }
 
     @NonNull
@@ -23,11 +31,22 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull TaskViewHolder holder, int position) {
+        cursor.moveToPosition(position);
+        holder.task.setText(cursor.getString(1));
+        holder.end_date.setText(cursor.getString(2));
+        String is_done = cursor.getString(3);
+        holder.remaining_time.setText(is_done);
+        if(is_done == "true"){
+            holder.isDone.setChecked(true);
+        } else{
+            holder.isDone.setChecked(false);
+        }
+
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return cursor.getCount();
     }
 }
