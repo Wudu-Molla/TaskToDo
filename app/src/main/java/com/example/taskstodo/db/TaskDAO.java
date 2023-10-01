9275a12312ae09740322b7695f0efa2e353eb292
Wudu-Molla <wudu.molla11@gmail.com>
2023-10-01 19:54:14 +0300

    add the Dao notation

--- app/src/main/java/com/example/taskstodo/db/TaskDAO.java
+++ app/src/main/java/com/example/taskstodo/db/TaskDAO.java
@@ -1,17 +1,20 @@
 package com.example.taskstodo.db;
 
+import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
 import java.util.List;
 
+@Dao
 public interface TaskDAO {
 
     @Query("SELECT * FROM tasks_table")
     List<Tasks> getTasks();
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(Tasks tasks);
 
     @Delete
