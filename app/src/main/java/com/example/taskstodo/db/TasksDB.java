326aaffec933f15d3b85f030dbf43901ac3ba113
Wudu-Molla <wudu.molla11@gmail.com>
2023-10-01 19:49:28 +0300

    changed the sqlite database to room database

--- app/src/main/java/com/example/taskstodo/db/TasksDB.java
+++ app/src/main/java/com/example/taskstodo/db/TasksDB.java
@@ -6,11 +6,28 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 import androidx.room.Database;
+import androidx.room.Insert;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @Database(entities = {Tasks.class}, version = 1)
 public abstract class TasksDB extends RoomDatabase {
 
     public abstract TaskDAO taskDAO();
+
+
+    public static TasksDB INSTANCE = null;
+
+    public static synchronized TasksDB getDatabase(Context context){
+
+        if(INSTANCE==null){
+            INSTANCE =  Room.databaseBuilder(context.getApplicationContext(),
+                    TasksDB.class, "tasks_database").allowMainThreadQueries().build();
+
+        }
+        return INSTANCE;
+
+    }
+
 
 }
