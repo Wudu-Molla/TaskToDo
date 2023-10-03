a3f9e76427b7c8a77c8092ff452823bdbfecf86c
Wudu-Molla <wudu.molla11@gmail.com>
2023-10-03 13:45:22 +0300

    added an update methode

--- app/src/main/java/com/example/taskstodo/db/TaskDAO.java
+++ app/src/main/java/com/example/taskstodo/db/TaskDAO.java
@@ -5,6 +5,7 @@ import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.List;
 
@@ -14,11 +15,17 @@ public interface TaskDAO {
     @Query("SELECT * FROM tasks_table")
     List<Tasks> getTasks();
 
+    @Query("SELECT * FROM tasks_table WHERE uid=:id")
+    List<Tasks> getTasksWithUid(int id);
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(Tasks tasks);
 
     @Delete
     void delete(Tasks tasks);
+
+    @Query("UPDATE tasks_table SET is_done=:isDone WHERE uid=:id")
+    void update(int id, boolean isDone);
 
 }
 
