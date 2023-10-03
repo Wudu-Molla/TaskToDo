54c8c351aa7a0126d7359a59b77c3d201ba9e0f0
Wudu-Molla <wudu.molla11@gmail.com>
2023-10-03 13:46:46 +0300

    added an callback on item clicked on the recycleview and add an interface

--- app/src/main/java/com/example/taskstodo/TaskAdapter.java
+++ app/src/main/java/com/example/taskstodo/TaskAdapter.java
@@ -1,26 +1,32 @@
 package com.example.taskstodo;
 
 import android.content.Context;
-import android.database.Cursor;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.taskstodo.db.TaskDAO;
 import com.example.taskstodo.db.Tasks;
+import com.example.taskstodo.db.TasksDB;
 
+import java.util.Calendar;
 import java.util.List;
 
-public class TaskAdapter extends RecyclerView.Adapter<TaskViewHolder> {
+public class TaskAdapter extends RecyclerView.Adapter<TaskViewHolder>{
 
-    private Context context;
+    private final Context context;
     List<Tasks> tasks;
+    TaskViewHolder viewHolder;
+    OnItemClicked onItemClicked;
 
 
-    public TaskAdapter(Context context, List<Tasks> tasks) {
+    public TaskAdapter(Context context, List<Tasks> tasks, OnItemClicked onItemClicked) {
         this.context = context;
         this.tasks = tasks;
+        this.onItemClicked = onItemClicked;
 
 
     }
@@ -28,7 +34,8 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskViewHolder> {
     @NonNull
     @Override
     public TaskViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new TaskViewHolder(LayoutInflater.from(context).inflate( R.layout.task_view, parent, false));
+        viewHolder =  new TaskViewHolder(LayoutInflater.from(context).inflate( R.layout.task_view, parent, false));
+        return viewHolder;
     }
 
     @Override
@@ -37,8 +44,16 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskViewHolder> {
         holder.task.setText(tasks.get(position).getTask());
         holder.end_date.setText(tasks.get(position).getEndDate());
         holder.isDone.setChecked(tasks.get(position).isDone);
-
-
+        if(tasks.get(position).isDone) {
+            holder.remaining_time.setText(R.string.done);
+        }else{
+            Calendar cal = Calendar.getInstance();
+            String now = String.valueOf(cal.getTime());
+            holder.remaining_time.setText(now);
+        }
+        holder.task_view.setOnClickListener(view ->{
+            onItemClicked.onItemClicked(tasks.get(position));
+        });
 
     }
 
@@ -46,4 +61,11 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskViewHolder> {
     public int getItemCount() {
         return tasks.size();
     }
+
+
+    public interface OnItemClicked{
+        void onItemClicked(Tasks task);
+    }
+
+
 }
