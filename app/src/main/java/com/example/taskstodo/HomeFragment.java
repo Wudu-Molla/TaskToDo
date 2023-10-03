9c0e027b047af261367ec988b78a428f7c20a62a
Wudu-Molla <wudu.molla11@gmail.com>
2023-10-03 13:50:51 +0300

    added and fixed the onclikitem handler

--- app/src/main/java/com/example/taskstodo/HomeFragment.java
+++ app/src/main/java/com/example/taskstodo/HomeFragment.java
@@ -1,8 +1,8 @@
 package com.example.taskstodo;
-
-import android.database.Cursor;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,14 +10,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.example.taskstodo.db.TaskDAO;
 import com.example.taskstodo.db.Tasks;
 import com.example.taskstodo.db.TasksDB;
 
-import java.util.List;
-
-public class HomeFragment extends Fragment{
+public class HomeFragment extends Fragment implements TaskAdapter.OnItemClicked {
     View view;
     RecyclerView recyclerView;
     TaskDAO instance;
@@ -36,12 +33,14 @@ public class HomeFragment extends Fragment{
         recyclerView = view.findViewById(R.id.task_collection);
         recyclerView.setLayoutManager(new LinearLayoutManager(requireActivity()));
         instance = TasksDB.getDatabase(requireActivity()).taskDAO();
-        List<Tasks> tasks = instance.getTasks();
-        recyclerView.setAdapter(new TaskAdapter(requireActivity(), tasks));
-        instance = null;
+        recyclerView.setAdapter(new TaskAdapter(requireActivity(), instance.getTasks(), this));
         return view;
     }
 
 
-
+    @Override
+    public void onItemClicked(Tasks task) {
+        instance.update(task.getUid(), !task.isDone());
+        recyclerView.setAdapter(new TaskAdapter(requireActivity(), instance.getTasks(), this));
+    }
 }
